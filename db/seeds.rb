6c553355e992e687f709ae911bf3039b4683537e
jokale <kalemajoanna@gmail.com>
2020-02-09 11:43:34 +0000

    create seed file and add total reset task

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+joanna = User.new 
+joanna.name = "Joanna"
+joanna.area = "Glutes"
+joanna.workout = "twenty reps of machine leg press, ten quick reps of stiff legged deadlift, twenty squat lifts"
+
+joanna.save 
